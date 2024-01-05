40b4bbae6f23fa283287c107e3aeefec3e7a9796
gloria <114293367@qq.com>
2024-01-04 23:38:32 -0800

    loaddata

--- scripts/load_data_pg.py
+++ scripts/load_data_pg.py
@@ -1,41 +1,40 @@
 import os
-import pg_util
+import pg_utils
+import psycopg2
 
 tables = [
 		'customer', 'customer_address',
 		'date_dim', 'item',
 		'store', 'store_sales']
 
-data_path = # directory of data files
-db_name = # database name
-bin_path = # path of the binary of Postgres
-tmp_csv_path = # path of tmp csv file for bulk loading
+# data_path = # directory of data files
+db_name = "dsb"
+# bin_path = # path of the binary of Postgres
+# tmp_csv_path = # path of tmp csv file for bulk loading
 
-create_db = True # If create the database
+create_db = False # If create the database
 create_table = True # If create the tables
 
 # start database service
-pg_util.start_server()
+# pg_util.start_server()
 
-# postgres credential
-user = 'postgres'
-password = 'admin'
 
-# create database
-if create_db:
-	# master_conn = pg_util.connect(user = user, password = password)
-	master_conn = pg_util.connect()
-	pg_util.execute(master_conn.cursor(), 'create database ' + db_name, verbose = True)
-	master_conn.close()
+# # create database
+# if create_db:
+# 	# master_conn = pg_util.connect(user = user, password = password)
+# 	master_conn = pg_util.connect()
+# 	pg_util.execute(master_conn.cursor(), 'create database ' + db_name, verbose = True)
+# 	master_conn.close()
 
 # connect to the database
-conn = pg_util.connect(user = user, password = password, db_name = db_name)
+conn = pg_utils.connect(user = user, password = password, db_name = db_name)
+# conn = psycopg2.connect(database=db_name, host="127.0.0.1", port=5432)
 cursor = conn.cursor()
 
 # create tables
 if create_table:
 	sql_path = r'../query_and_templates/create_tables.sql'
-	pg_util.execute(cursor, open(sql_path, 'r').read(), verbose = True)
+	cursor.execute(open(sql_path, 'r').read())
 
 # # insert tuples into tables
 # for table in tables:
